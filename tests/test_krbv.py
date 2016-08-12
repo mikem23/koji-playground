8b5fcc40babe7040ba9fb94d47c08cca69d170f1
Mike McLean <mikem@redhat.com>
2016-08-12 15:05:37 -0400

    update krb unit test

--- tests/test_krbv.py
+++ tests/test_krbv.py
@@ -10,8 +10,8 @@ class KrbVTestCase(unittest.TestCase):
 
     @mock.patch('koji.krbV', new=None)
     @mock.patch('koji.gssapi', new=None)
-    @mock.patch('koji.ClientSession._setup_connection')
-    def test_krbv_disabled(self, _setup_connection):
+    @mock.patch('koji.ClientSession._setup_connection', new=mock.MagicMock())
+    def test_krbv_disabled(self):
         """ Test that when krb libs are absent, we behave rationally. """
         self.assertEquals(koji.krbV, None)
         self.assertEquals(koji.gssapi, None)
@@ -20,19 +20,35 @@ class KrbVTestCase(unittest.TestCase):
             session.krb_login()
 
     @mock.patch('koji.krbV', new=None)
-    @mock.patch('koji.gssapi', new=True)
+    @mock.patch('koji.gssapi', new=None)
+    @mock.patch('koji.ClientSession._setup_connection', new=mock.MagicMock())
+    @mock.patch('koji.ClientSession._callMethod', new=mock.MagicMock())
+    @mock.patch('koji.ClientSession.logout', new=mock.MagicMock())
     @mock.patch('koji.ClientSession.krb_gssapi_login')
     @mock.patch('koji.ClientSession.krb_krbV_login')
-    @mock.patch('koji.ClientSession._setup_connection')
-    @mock.patch('koji.ClientSession._callMethod')
-    def test_krbv_disabled(self, _callMethod, _setup_connection, krb_krbV_login, krb_gssapi_login):
-        """ Test that gssapi codepath gets used """
-        #import pdb; pdb.set_trace()
-        self.assertEquals(koji.krbV, None)
-        self.assertEquals(koji.gssapi, True)
-        krb_gssapi_login.return_value = '23 fnord'  #session id and key
-        session = koji.ClientSession('whatever')
+    def test_krbv_disabled(self, krb_krbV_login, krb_gssapi_login):
+        """ Test that correct krb codepath is used """
+
+        mocks = (krb_krbV_login, krb_gssapi_login)
+        sinfo = {'session-id': '23', 'session-key': 'fnord'}
+        sinfo_str = '23 fnord'
         login_args = ('principal', 'keytab', 'ccache', 'proxyuser')
-        session.krb_login(*login_args)
-        krb_krbV_login.assert_not_called()
-        krb_gssapi_login.assert_called_with(*login_args)
+
+        with mock.patch('koji.gssapi', new=True):
+            krb_gssapi_login.return_value = sinfo_str
+            session = koji.ClientSession('whatever')
+            session.krb_login(*login_args)
+            krb_krbV_login.assert_not_called()
+            krb_gssapi_login.assert_called_with(*login_args)
+            assert session.sinfo == sinfo
+
+        for m in mocks:
+            m.reset_mock()
+
+        with mock.patch('koji.krbV', new=True):
+            krb_krbV_login.return_value = sinfo_str
+            session = koji.ClientSession('whatever')
+            session.krb_login(*login_args)
+            krb_gssapi_login.assert_not_called()
+            krb_krbV_login.assert_called_with(*login_args)
+            assert session.sinfo == sinfo
