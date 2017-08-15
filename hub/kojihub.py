c0e89b636b84ae81171d4f78c9f88f96f44a314e
Mike McLean <mikem@redhat.com>
2017-08-15 11:26:20 -0400

    fix br data

--- hub/kojihub.py
+++ hub/kojihub.py
@@ -5058,7 +5058,7 @@ class RPMBuildImporter(object):
         self.metadata['build'] = bdata
 
     def get_buildroot_data(self):
-        brdata = dict([(n, n) for n in self.brmap.values()])
+        brdata = [dict.fromkeys(['id', 'koji_buildroot_id'], n) for n in self.brmap.values()]
         self.metadata['buildroots'] = brdata
 
     def get_build_output(self):
