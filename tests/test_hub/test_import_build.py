9071d66c42e1bf7de6c199e279d09b31d12d0ca1
Mike McLean <mikem@redhat.com>
2017-08-15 17:54:14 -0400

    partial refactor of TestImportBuild
    passes, but no assertions yet

--- tests/test_hub/test_import_build.py
+++ tests/test_hub/test_import_build.py
@@ -165,9 +165,36 @@ class TestImportRPM(unittest.TestCase):
         _dml.assert_called_once_with(statement, values)
 
 
+QP = kojihub.QueryProcessor
+
+
 class TestImportBuild(unittest.TestCase):
+
     def setUp(self):
         self.tempdir = tempfile.mkdtemp()
+        self.queries = []
+        self.QueryProcessor = mock.patch('kojihub.QueryProcessor',
+                side_effect=self.get_query).start()
+        self.importer = mock.MagicMock()
+        self.CG_Importer = mock.patch('kojihub.CG_Importer',
+                new=self.importer).start()
+        self.Task_getInfo = mock.MagicMock()
+        self.Task = mock.patch('kojihub.Task', new=self.Task_getInfo).start()
+        self.get_header_fields = mock.patch('koji.get_header_fields').start()
+        self.exports = kojihub.RootExports()
+        self.setup_rpm()
+
+    def tearDown(self):
+        mock.patch.stopall()
+        shutil.rmtree(self.tempdir)
+
+    def get_query(self, *args, **kwargs):
+        query = QP(*args, **kwargs)
+        query.execute = mock.MagicMock()
+        self.queries.append(query)
+        return query
+
+    def setup_rpm(self):
         self.filename = self.tempdir + "/name-version-release.arch.rpm"
         # Touch a file
         with open(self.filename, 'w'):
@@ -177,105 +204,17 @@ class TestImportBuild(unittest.TestCase):
         with open(self.src_filename, 'w'):
             pass
 
-        self.rpm_header_retval = {
-            'filename': 'name-version-release.arch.rpm',
-            1000: 'name',
-            1001: 'version',
-            1002: 'release',
-            1003: 'epoch',
-            1006: 'buildtime',
-            1022: 'arch',
-            1044: 'name-version-release.arch',
-            1106: 'sourcepackage',
-            261: 'payload hash',
-        }
-
-    def tearDown(self):
-        shutil.rmtree(self.tempdir)
-
-    @mock.patch('kojihub.check_volume_policy')
-    @mock.patch('kojihub.new_typed_build')
-    @mock.patch('kojihub._dml')
-    @mock.patch('kojihub._singleValue')
-    @mock.patch('kojihub.get_build')
-    @mock.patch('kojihub.add_rpm_sig')
-    @mock.patch('koji.rip_rpm_sighdr')
-    @mock.patch('kojihub.import_rpm_file')
-    @mock.patch('kojihub.import_rpm')
-    @mock.patch('kojihub.QueryProcessor')
-    @mock.patch('kojihub.context')
-    @mock.patch('kojihub.new_package')
-    @mock.patch('koji.get_rpm_header')
-    @mock.patch('koji.pathinfo.work')
-    def test_import_build_completed_build(self, work, get_rpm_header,
-            new_package, context, query, import_rpm, import_rpm_file,
-            rip_rpm_sighdr, add_rpm_sig, get_build, _singleValue, _dml,
-            new_typed_build, check_volume_policy):
-
-        rip_rpm_sighdr.return_value = (0, 0)
-        check_volume_policy.return_value = {'id': 0, 'name': 'DEFAULT'}
-
-        processor = mock.MagicMock()
-        processor.executeOne.return_value = None
-        query.return_value = processor
-
-        context.session.user_id = 99
-
-        work.return_value = '/'
-
-        retval = copy.copy(self.rpm_header_retval)
-        retval.update({
-            'filename': 'name-version-release.arch.rpm',
-            1044: 'name-version-release.src',
-            1022: 'src',
-            1106: 1,
-        })
-        get_rpm_header.return_value = retval
-        binfo = {
-            'state': koji.BUILD_STATES['COMPLETE'],
+        self.get_header_fields.return_value = {
+            'sourcepackage': 1,
             'name': 'name',
             'version': 'version',
             'release': 'release',
-            'id': 12345,
+            'arch': 'arch',
+            'epoch': 'epoch',
+            'buildtime': 'buildtime',
+            'sigmd5': 'sigmd5',
         }
-        # get_build called once to check for existing,
-        # then later to get the build info
-        get_build.side_effect = [None, binfo]
+
+    def test_import_build_completed_build(self):
 
         kojihub.import_build(self.src_filename, [self.filename])
-
-        fields = [
-            'task_id',
-            'extra',
-            'start_time',
-            'epoch',
-            'completion_time',
-            'state',
-            'version',
-            'source',
-            'volume_id',
-            'owner',
-            'release',
-            'pkg_id',
-            'id',
-        ]
-        statement = 'INSERT INTO build (%s) VALUES (%s)' % (
-            ", ".join(fields),
-            ", ".join(['%%(%s)s' % field for field in fields])
-        )
-        values = {
-            'task_id': None,
-            'extra': None,
-            'start_time': 'NOW',
-            'epoch': 'epoch',
-            'completion_time': 'NOW',
-            'state': 1,
-            'version': 'version',
-            'source': None,
-            'volume_id': 0,
-            'owner': 99,
-            'release': 'release',
-            'pkg_id': mock.ANY,
-            'id': mock.ANY,
-        }
-        _dml.assert_called_once_with(statement, values)
