54d07725f86355fcafb91f49c3164fb805161517
Mike McLean <mikem@redhat.com>
2017-09-27 02:25:18 -0400

    naive completeMavenBuild test is passing

--- tests/test_hub/test_complete_maven_build.py
+++ tests/test_hub/test_complete_maven_build.py
@@ -10,6 +10,9 @@ import koji
 import kojihub
 
 
+orig_import_archive_internal = kojihub.import_archive_internal
+
+
 class TestCompleteMavenBuild(unittest.TestCase):
 
     def setUp(self):
@@ -26,8 +29,11 @@ class TestCompleteMavenBuild(unittest.TestCase):
         self.get_maven_build = mock.patch('kojihub.get_maven_build').start()
         self.get_archive_type = mock.patch('kojihub.get_archive_type').start()
         mock.patch('kojihub.lookup_name', new=self.my_lookup_name).start()
+        mock.patch('kojihub.import_archive_internal',
+                    new=self.my_import_archive_internal).start()
         mock.patch('kojihub._dml').start()
         mock.patch('kojihub._fetchSingle').start()
+        mock.patch('kojihub.build_notification').start()
 
     def tearDown(self):
         mock.patch.stopall()
@@ -46,6 +52,10 @@ class TestCompleteMavenBuild(unittest.TestCase):
                 src = os.path.join(datadir, subdir, fn)
                 dst = os.path.join(path, fn)
                 shutil.copy(src, dst)
+        for fn in data['logs']:
+            src = os.path.join(datadir, fn)
+            dst = os.path.join(taskdir, fn)
+            shutil.copy(src, dst)
         self.maven_data = data
 
     def my_lookup_name(self, table, info, **kw):
@@ -53,6 +63,20 @@ class TestCompleteMavenBuild(unittest.TestCase):
             return mock.MagicMock()
         else:
             raise Exception("Cannot fake call")
+
+    def my_import_archive_internal(self, *a, **kw):
+        # this is kind of odd, but we need this to fake the archiveinfo
+        share = {}
+        def my_ip(table, *a, **kw):
+            if table == 'archiveinfo':
+                share['archiveinfo'] = kw['data']
+                # TODO: need to add id
+            return mock.MagicMock()
+        def my_ga(archive_id, **kw):
+            return share['archiveinfo']
+        with mock.patch('kojihub.InsertProcessor', new=my_ip), \
+                    mock.patch('kojihub.get_archive', new=my_ga):
+            orig_import_archive_internal(*a, **kw)
 
     def set_up_callbacks(self):
         new_callbacks = copy.deepcopy(koji.plugin.callbacks)
@@ -69,7 +93,3 @@ class TestCompleteMavenBuild(unittest.TestCase):
         buildinfo = koji.maven_info_to_nvr(self.maven_data['maven_info'])
         buildinfo['release'] = 1
         self.hostcalls.completeMavenBuild('TASK_ID', 'BUILD_ID', self.maven_data, None)
-
-
-
-
