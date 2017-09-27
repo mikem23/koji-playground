b6485ce23d989700bb916279101748df9c9d3fcf
Mike McLean <mikem@redhat.com>
2017-09-27 01:42:22 -0400

    add mocks needed for unit test

--- tests/test_hub/test_complete_maven_build.py
+++ tests/test_hub/test_complete_maven_build.py
@@ -16,6 +16,18 @@ class TestCompleteMavenBuild(unittest.TestCase):
         self.tempdir = tempfile.mkdtemp()
         self.pathinfo = koji.PathInfo(self.tempdir)
         mock.patch('koji.pathinfo', new=self.pathinfo).start()
+        self.hostcalls = kojihub.HostExports()
+        self.context = mock.patch('kojihub.context').start()
+        self.context.opts = {'EnableMaven': True}
+        mock.patch('kojihub.Host').start()
+        self.Task = mock.patch('kojihub.Task').start()
+        self.Task.return_value.assertHost = mock.MagicMock()
+        self.get_build = mock.patch('kojihub.get_build').start()
+        self.get_maven_build = mock.patch('kojihub.get_maven_build').start()
+        self.get_archive_type = mock.patch('kojihub.get_archive_type').start()
+        mock.patch('kojihub.lookup_name', new=self.my_lookup_name).start()
+        mock.patch('kojihub._dml').start()
+        mock.patch('kojihub._fetchSingle').start()
 
     def tearDown(self):
         mock.patch.stopall()
@@ -34,6 +46,13 @@ class TestCompleteMavenBuild(unittest.TestCase):
                 src = os.path.join(datadir, subdir, fn)
                 dst = os.path.join(path, fn)
                 shutil.copy(src, dst)
+        self.maven_data = data
+
+    def my_lookup_name(self, table, info, **kw):
+        if table == 'btype':
+            return mock.MagicMock()
+        else:
+            raise Exception("Cannot fake call")
 
     def set_up_callbacks(self):
         new_callbacks = copy.deepcopy(koji.plugin.callbacks)
@@ -47,5 +66,10 @@ class TestCompleteMavenBuild(unittest.TestCase):
 
     def test_complete_maven_build(self):
         self.set_up_files('import_1')
+        buildinfo = koji.maven_info_to_nvr(self.maven_data['maven_info'])
+        buildinfo['release'] = 1
+        self.hostcalls.completeMavenBuild('TASK_ID', 'BUILD_ID', self.maven_data, None)
+
+
 
 
